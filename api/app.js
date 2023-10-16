1660ac3f6b3ad4d4545f5b2d925ba49e8f3e0a99
sihamyoubi <sihamyoubi5@gmail.com>
2023-10-16 09:44:57 +0100

    changes in app.js and update routes

--- api/app.js
+++ api/app.js
@@ -5,6 +5,8 @@ const passport = require('passport');
 
 const connectDb = require('./config/database');
 const PORT = require('./config/env').PORT;
+const indexRoutes = require('./routes/index.routes')
+
 
 // ------ Middlewares ------
 connectDb();
@@ -14,18 +16,9 @@ app.use(express.json());
 app.use(passport.initialize());
 require('./middleware/passport');
 
-// Routes
-const userRoutes = require('./routes/userRoutes');
-const protectedRoutes = require('./routes/protectedRoutes');
-const categoryRoutes = require('./routes/categoryRoutes.js');
-const subcategoryRoutes = require('./routes/subcategoryRoutes.js');
-const customerRoutes = require('./routes/customerRoutes');
 
-app.use('/v1/users', userRoutes);
-app.use('/v1/customers', customerRoutes);
-app.use('/v1', protectedRoutes);
-app.use('/v1/subcategories', subcategoryRoutes)
-app.use('/v1/categories', categoryRoutes)
+app.use('/v1', indexRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
