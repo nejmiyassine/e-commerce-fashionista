c52aa0f7f807d14a55940530a139aa0d834397b5
nejmiyassine <devtwins0@gmail.com>
2023-10-23 09:30:13 +0100

    Install redux toolkit and configure our store

---
+++ client/src/app/rootReducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from '@reduxjs/toolkit';
+// import feature1Reducer from '../features/feature1/feature1Slice';
+// import feature2Reducer from '../features/feature2/feature2Slice';
+
+const rootReducer = combineReducers({
+    // feature1: feature1Reducer,
+    // feature2: feature2Reducer,
+});
+
+export default rootReducer;
