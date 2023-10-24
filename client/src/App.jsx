bcb28d07a4a92924439ad5c11fdacca3cfc2424d
sihamyoubi <sihamyoubi5@gmail.com>
2023-10-24 14:19:23 +0100

    nothing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -26,8 +27,12 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and
+         React logos to learn more
       </p>
+
+    
+      
     </>
   )
 }
