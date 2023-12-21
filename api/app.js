43062ae9f76df4b1a21542054c219be996dbbe19
nejmiyassine <devtwins0@gmail.com>
2023-12-21 22:00:30 +0100

    Cors options change links

--- api/app.js
+++ api/app.js
@@ -12,7 +12,6 @@ const indexRoutes = require('./routes/index.routes');
 
 const corsOptions = {
     origin: ['http://localhost:5173', BASE_URL],
-    default: 'http://localhost:5173',
     credentials: true,
     methods: 'GET, POST, PUT,DELETE, PATCH',
 };
